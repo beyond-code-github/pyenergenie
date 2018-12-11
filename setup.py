e3d3b22f51431dd8eb05bc43fe1bb598f9f10bf2
Pete Smith <github@beyond-code.com>
2018-12-11 16:50:49 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from distutils.core import setup, find_packages
+from setuptools import setup, find_packages
 
 setup(
     name='pyenergenie',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     license='MIT',
 )
