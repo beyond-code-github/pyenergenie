a9b76b11c0553f29ba218a56823eb8f7ce490000
Pete Smith <github@beyond-code.com>
2018-12-11 18:43:57 +0000

    Added non-python files to pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyenergenie',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
+    include_package_data=True,
     license='MIT',
 )
